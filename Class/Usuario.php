67cc38d6050b9d95de575370017d78f4a955b3c8
dromdark <carlosmiguelsouza2011@gmail.com>
2018-09-14 16:09:24 -0300

    usuario

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -28,6 +28,7 @@ class Usuario{
 	public function setDtcadastro($value){
 		$this->dtcadastro=$value;
 	}
+	//o loadId carrega um usuario
 	public function loadId($id){
 		$model=new Model();
 		$resultado=$model->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(":ID"=>$id));
@@ -40,13 +41,43 @@ class Usuario{
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
 	}
+	//pega todos os valores da tabela,uma lista de usuarios
+	public function getList(){
+		$model=new Model();
+		return $model->select("SELECT * FROM tb_usuarios ORDER BY idusuario");
+	}
+	//procurar por login
+	public function search($login){
+		$model=new Model();
+		return $model->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH",array(":SEARCH"=>"%".$login."%"));
+	}
+	//função de login
+	public function login($login,$password){
+		$model=new Model();
+		$resultado=$model->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(":LOGIN"=>$login,":PASSWORD"=>$password));
+		if (count($resultado)>0) {
+			$row=$resultado[0];
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			//colocar em formato data e hora
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+		else{
+			throw new Exception("Login ou senha inválido");
+			
+		}
+	}
+	//imprime 
 	public function __toString(){
-		return json_encode(array(
+		return json_encode(
+			array(
 			"idusuario"=>$this->getIdusuario(),
 			"deslogin"=>$this->getDeslogin(),
 			"dessenha"=>$this->getDessenha(),
 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-		));
+			)
+		);
 	}
 
 }
--- index.php
+++ index.php
@@ -4,7 +4,14 @@ require_once("config.php");
 //$usuarios=$mdl->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 $usuario=new Usuario();
-$usuario->loadId(3);
+//$usuario->loadId(3);
+//carrega um usuario especifico
+//echo $usuario;
+//carrega todos usuarios
+//echo json_encode($usuario->getList());
+//carrega uma lista de usuarios buscando pelo isuario
+//echo json_encode($usuario->search("ca"));
+//carrega um usuario usando o login ou a senha
+$usuario->login("ca2","213214");
 echo $usuario;
-
  ?>
